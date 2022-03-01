73dbb54c87ce8602a3ca58cb8fb84c874ac9a2e4
Seunghyun100 <87792403+Seunghyun100@users.noreply.github.com>
2022-03-01 21:36:53 +0900

    make a method to calculate heating rate of closed system

--- plot_heating_rate/plot_heating_rate.py
+++ plot_heating_rate/plot_heating_rate.py
@@ -65,6 +65,29 @@ class HeatingRate:
             for j in range(upto_t+1):
                 if j != 0:
                     pre_k = data[i][j-1]
+                    k = (i/(i+m))*(pre_k + 5.2) + 1.1
+                    data[i].append(k)
+
+        self.export_csv_data(file_name=name, data=data)
+        return data
+
+    def calculate_qbus_ratio(self, upto_n: int = 10, upto_t: int = 50, ratio: tuple = (1, 1),
+                             k_init: float = 0, name: str = "Q-bus_heating_rate"):
+        data = {}
+
+        for i in range(upto_n):
+            i += 1
+            i = i*ratio[0]  # initial n = 1 * ratio[0]
+            # it means k0(initial #phonon) where n = i
+            data[i] = [k_init]
+
+        for i in range(upto_n):
+            i += 1
+            i = i*ratio[0]
+            m = i*ratio[1]
+            for j in range(upto_t+1):
+                if j != 0:
+                    pre_k = data[i][j-1]
                     k = (i/(i+m))*(pre_k + 4.2) + 1
                     data[i].append(k)
 
@@ -148,6 +171,7 @@ class HeatingRate:
             y_axis = range(len(x_axis))
         plt.plot(y_axis, x_axis)
         plt.xlabel("#Shuttling")
+        plt.xlim(0, 50)
         plt.ylabel("#Phonon")
         plt.title(f"{n} Qubits per core")
 
@@ -199,6 +223,50 @@ class HeatingRate:
         # self.export_plot_data(data=fig, file_name=name)
         plt.show()
 
+    def plot_ratio_3d(self, *data, ratio: tuple = (1, 1), name: str = "no_name_3d"):
+        fig, ax = plt.subplots(ncols=1, figsize=(10, 10),
+                                subplot_kw={"projection": "3d"})
+
+        fontlabel = {"fontsize": "large", "color": "gray", "fontweight": "bold"}
+
+        ax.set_xlabel("#qubit per core", fontdict=fontlabel, labelpad=1)
+        ax.set_ylabel('#shuttling', fontdict=fontlabel, labelpad=1)
+        ax.set_title("#phonon with constant ratio", fontdict=fontlabel)
+        ax.view_init(elev=20, azim=160)  # 각도 지정
+
+        cmap_count = 0
+        cmap_list = ['Blues', 'Greens', 'inferno']
+
+        for data_i in data:
+            # make data frame
+            n = len(data_i.keys()) -1
+            t = len(data_i) -1
+
+            x = np.array(range(1, n+1))  # X axis is #qubit per core
+            y = np.array(range(t+1))  # Y axis is #shuttling
+            z = []  # Z axis is #phonon
+            x, y = np.meshgrid(x, y)
+            x = x.reshape(n*(t+1))
+            y = y.reshape(n*(t+1))
+            for i in range(len(x)):
+                z.append(data_i[f'{x[i]*ratio[0]}'][y[i]])
+
+            df = ps.DataFrame({'x': x*ratio[0], 'y': y, 'z': z})
+            ax.plot_trisurf(df.x, df.y, df.z, cmap=f'{cmap_list[cmap_count]}', alpha=0.9, linewidth=0.1)
+            cmap_count += 1
+
+        # save the figure
+        file_name = f'{current_path}/data/heating_rate/plot_data/{name}'
+        no = 1
+        while os.path.exists(f'{file_name}({no}).png'):  # if already exists same name file
+            no += 1
+        # save to png file
+        fig.savefig(f'{file_name}({no}).png', dpi=600)
+
+        # self.export_plot_data(data=fig, file_name=name)
+        plt.show()
+
+
     def plot_contour_line(self, data, name):
 
         fig, ax = plt.subplots(ncols=1, figsize=(10, 10))
@@ -240,11 +308,13 @@ if __name__ == '__main__':
 
     test = HeatingRate()
     # test.calculate_qccd(upto_n=27, upto_t=1000, m=12, name="QCCD_heating_rate_1000_2.25")
-    # test.calculate_qbus(upto_n=27, upto_t=1000, m=12, name="Q-bus_heating_rate_1000_2.25")
+    test.calculate_qbus(upto_n=32, upto_t=1000, m=4, name="Q-bus_heating_rate_m4_1000_2.26")
+    # test.calculate_qbus_ratio(upto_n=10, upto_t=1000, ratio=(9, 4), name="Q-bus_constant_ratio_heating_rate_1000_2.25")
     # test.calculate_closed_system(upto_n=50, upto_t=1000, m=1, name="closed_sytem_heating_rate_1000_2.25")
 
-    test_data1 = test.read_csv_data("Q-bus_heating_rate_1000_2.25.csv")
-    test_data2 = test.read_csv_data("QCCD_heating_rate_1000_2.25_core1.csv")
+    # test_data1 = test.read_csv_data("Q-bus_constant_ratio_heating_rate_1000_2.25.csv")
+    test_data1 = test.read_csv_data("Q-bus_heating_rate_m4_1000_2.26.csv")
+    # test_data2 = test.read_csv_data("QCCD_heating_rate_1000_2.25_core1.csv")
     # test_data3 = test.read_csv_data("QCCD_heating_rate_1000_2.22_core2.csv")
     # test_data4 = test.read_csv_data("closed_sytem_heating_rate_1000_2.25.csv")
 
@@ -254,16 +324,17 @@ if __name__ == '__main__':
     # test.plot_contour_line(data=test_data, name="comparing_n27_m12_2.25")
 
     # 2D plot
-    # q_bus_quanta = test_data1["20"]
+    q_bus_quanta = test_data1["32"]
     # qccd_quanta = test_data2["30"]
     # closed_system = test_data4["30"]
-    # test.plot_2d(list(q_bus_quanta), n=20, name="q_bus_n_20")
+    test.plot_2d(list(q_bus_quanta), n='n(32) m(4)', name="q_bus_n32_m4")
     # test.plot_2d(list(qccd_quanta), n=30, name="qccd_n_30")
     # test.plot_2d(list(closed_system), n=30, name="closed_system_n_30")
 
     # 3D plot
-    test.plot_3d(test_data1, test_data2, name="Q-bus_vs_QCCD_n27_3d_plot_2.25")
-    # test.plot_3d(test_data1, name="Q-bus_heating_rate_1000_2.25")
+    # test.plot_3d(test_data1, test_data2, name="Q-bus_vs_QCCD_n27_3d_plot_2.25")
+    # test.plot_3d(test_data1, name="Q-bus_heating_rate_1000_2.26")
+    # test.plot_ratio_3d(test_data1, ratio=(9, 4), name="Q-bus_constant_ratio_heating_rate_1000_2.25")
 
 
 
